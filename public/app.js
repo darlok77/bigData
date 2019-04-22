cb411e950f350b1cfb92053c9faf2b0ac74d65a9
darlok77 <g.dalleau77@outlook.fr>
2019-04-22 12:40:26 +0200

    refractor(fix eslint error)

--- public/app.js
+++ public/app.js
@@ -1,98 +1,21 @@
 const fs = require('fs')
-const path = './mock'
 const pm2 = require('pm2')
-// ICI trow err
 
-/* class Test {
-  getNameSplitFile() {
-    return new Promise((resolve) => {
-      fs.readdir(path, (err, items) => {
-        if (err) {
-          console.log(err)
-        }
-        const fileTab = []
-
-        for (var i = 0; i < items.length; i ++) {
-          if (items[i] !== '.DS_Store') {
-            fileTab.push(items[i])
-          }
-        }
-        resolve(fileTab)
-      })
-    })
-  }
-
-  startProcess(fileTab) {
-    pm2.connect(err => {
-      if (err) {
-        console.error(err)
-        process.exit(2)
-      }
-      pm2.start({
-        'script': 'public/save.js' // Script to be run
-        instances: 4
-        // 'exec_mode': 'cluster'
-      }, (err, apps) => {
-        if (err) {
-          console.log(err)
-        }
-        console.log('app started!')
-        console.log(apps)
-        pm2.list((err, list) => {
-          if (err) {
-            console.log(err)
-          }
-          pm2.sendDataToProcessId({
-            'id': apps.pm2_env.pm_id,
-            'type': 'process:msg',
-            'data': {
-              'file': 'mock.js',
-            },
-            'topic': 'my topic'
-          },
-          (err, res) => {
-            if (err) {
-              console.log(err)
-            }
-          })
-          // pm2.disconnect()
-        })
-      })
-    })
-  }
-  run() {
-    // this.getNameSplitFile ()
-    this.getNameSplitFile().then((res) => {
-      this.startProcess(res)
-    })
-  }
-} */
-
-//const test = new Test()
-
-//test.run()
-/* pm2.launchBus((err, bus) => {
-  bus.on('process:msg', packet => {
-    console.log('test')
-    packet.data.success.should.eql(true)
-    packet.process.pm_id.should.eql(proc1.pm2_env.pm_id)
-    done()
-  })
-}) */
+const path = './split'
 
 /*
 * getNameSplitFile
 * @return {array} fileTab
 */
-const getNameSplitFile = () => {
-  return new Promise((resolve) => {
+const getNameSplitFile = () => (
+  new Promise((resolve) => {
     fs.readdir(path, (err, items) => {
       if (err) {
         console.log(err)
       }
       const fileTab = []
 
-      for (var i = 0; i < items.length; i ++) {
+      for (let i = 0; i < items.length; i += 1) {
         if (items[i] !== '.DS_Store') {
           fileTab.push(items[i])
         }
@@ -100,50 +23,45 @@ const getNameSplitFile = () => {
       resolve(fileTab)
     })
   })
-}
+)
 
 /*
-* startProcess
-* @param {array} fileTab 
-* @param {int} processId 
+*startProcess
+* @param {array} fileTab
+* @param {int} processId
 */
 const startProcess = (fileTab, processId) => {
-  console.log('ABBBA')
-  console.log(processId)
   const id = processId + 1
-  pm2.connect(err => {
+  pm2.connect((err) => {
     if (err) {
       console.error(err)
       process.exit(2)
     }
     pm2.start({
-      'script': 'public/save.js' // Script to be run
+      script: 'public/save.js' // Script to be run
       // 'exec_mode': 'cluster'
-    }, (err, apps) => {
-      if (err) {
-        console.log(err)
+    }, (errStart) => {
+      if (errStart) {
+        console.log(errStart)
       }
-      //console.log(apps)
+      // console.log(apps)
       console.log('app started!')
-      pm2.list((err, list) => {
-        if (err) {
-          console.log(err)
+      pm2.list((errList) => {
+        if (errList) {
+          console.log(errList)
         }
-        console.log('----------------------')
-        console.log(list.length)
-        console.log(list[list.length-1].pm2_env.pm_id)
         pm2.sendDataToProcessId({
-          'id': id,
-          'type': 'process:msg',
-          'data': {
-            'file': fileTab[0],
-            'totalFile' :fileTab
+          id,
+          type: 'process:msg',
+          data: {
+            file: fileTab[0],
+            totalFile: fileTab
           },
-          'topic': 'my topic'
+          topic: 'my topic'
         },
-        (err, res) => {
-          if (err) {
-            console.log(err)
+        (errSendData, res) => {
+          if (errSendData) {
+            console.log(errSendData)
           }
           console.log(res)
         })
@@ -153,13 +71,12 @@ const startProcess = (fileTab, processId) => {
   })
 }
 getNameSplitFile().then((res) => {
-  startProcess(res, 0)
+  startProcess(res, 0) // 2 lunch wtf
 })
 
 pm2.launchBus((err, bus) => {
-  bus.on('messageToParent', packet => {
+  bus.on('messageToParent', (packet) => {
     pm2.delete(packet.process.pm_id)
-    console.log(packet.data.totalFile.length)
     if (packet.data.totalFile.length !== 0) {
       startProcess(packet.data.totalFile, packet.process.pm_id)
     }
--- public/save.js
+++ public/save.js
@@ -1,60 +1,56 @@
 const csv = require('csvtojson')
-const MongoClient = require('mongodb').MongoClient
+const mongo = require('mongodb').MongoClient
 
+const { MongoClient } = mongo
 const url = 'mongodb://localhost:27017/bigDataGD'
-const filename = 'mock.csv'
 
 /*
 * createNewEntries
-* @param {Object} client 
-* @param {array} entries 
+* @param {Object} client
+* @param {array} entries
 * @return {object} client
 */
 const createNewEntries = (client, entries) => (
-  new Promise(resolve => {
+  new Promise((resolve) => {
     // Get the collection and bulk api artefacts
     const db = client.db('bigDataGD')
     const collection = db.collection('entries')
     let bulkUpdateOps = []
 
-    entries.forEach(doc => {
-      bulkUpdateOps.push({ "insertOne": { "document": doc } })
+    entries.forEach((doc) => {
+      bulkUpdateOps.push({ insertOne: { document: doc } })
       if (bulkUpdateOps.length === 1000) {
-        collection.bulkWrite(bulkUpdateOps).then(r => {
-          // do something with result
-        })
+        collection.bulkWrite(bulkUpdateOps)
         bulkUpdateOps = []
       }
     })
     if (bulkUpdateOps.length > 0) {
-      collection.bulkWrite(bulkUpdateOps).then(r => {
-      })
+      collection.bulkWrite(bulkUpdateOps)
     }
     resolve(client)
   })
 )
 
-process.on('message', data => {
+process.on('message', (data) => {
   console.log(data.data.file)
   csv()
-  .fromFile(`mock/${data.data.file}`)
-  .then(jsonObj => {
-    MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
-      createNewEntries(client, jsonObj).then((res) => {
-        res.close()
-        console.log('ici')
-        console.log(`mock/${data.data.file}`)
-        posFile = data.data.totalFile.indexOf(data.data.file)
-        remainingFile = data.data.totalFile.splice(posFile, 1)
-        process.send({
-          'type': 'messageToParent',
-          'data': {
-            'success': true,
-            'totalFile': remainingFile
-          }
+    .fromFile(`mock/${data.data.file}`)
+    .then((jsonObj) => {
+      MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
+        createNewEntries(client, jsonObj).then((res) => {
+          res.close()
+          console.log('ici')
+          console.log(`mock/${data.data.file}`)
+          const posFile = data.data.totalFile.indexOf(data.data.file)
+          const remainingFile = data.data.totalFile.splice(posFile, 1)
+          process.send({
+            type: 'messageToParent',
+            data: {
+              totalFile: remainingFile
+            }
+          })
         })
       })
     })
-  })
 })
 
